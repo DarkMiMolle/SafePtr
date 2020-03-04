48d2315619b5239d82cacf896135b36b393e6e52
Florent Carrez <florent.carrez@epita.fr>
2020-03-04 13:28:27 +0100

    implement operator and bad_ptr_access exception

--- SafePtr/Ptr.hpp
+++ SafePtr/Ptr.hpp
@@ -4,6 +4,12 @@
 #include <memory>
 #include <map>
 #include <vector>
+#include <exception>
+
+class bad_access_ptr: std::exception {
+public:
+    const char* what() const noexcept override { return "can't access  to  nullptr";}
+};
 
 template <typename T>
 class Ptr {
@@ -13,7 +19,9 @@ protected:
   T* m_ptr = nullptr;
   size_t m_id = -1;
 
+    /// \def list is a map of all T* pointers and the associated Ptr to that pointer.
   inline static std::map<T*, std::vector<Ptr*>> list;
+
 
   void pre_dtor() {
     if (m_ptr) {
@@ -29,6 +37,7 @@ protected:
     }
   }
 
+  /// \brief make the pointer
   Ptr(T* ptr, bool del): m_ptr(ptr), toDelete(del) {
     newest = true;
     if (!list.count(ptr)) {
@@ -40,7 +49,7 @@ protected:
   }
 public:
 
-  Ptr() = default;
+  Ptr() = default; /// \brief set Ptr as nullptr
   Ptr(const Ptr& copy);
   Ptr(const Ptr&& mv);
 
@@ -52,9 +61,13 @@ public:
   bool operator==(const Ptr&& ptr) const;
   bool operator==(const T* ptr) const;
 
-  Ptr& operator+(const int i);
-  Ptr& operator-(const int i);
+  /// \brief classical + and - operator for pointers
+  Ptr operator+(const int i) const ;
+  Ptr operator-(const int i) const ;
+  Ptr& operator++();
+  Ptr& operator--();
 
+  /// \brief return whether or not pointer is nullptr
   operator bool () const;
 
   const T* operator ->() const;
@@ -64,7 +77,7 @@ public:
   T& operator * ();
 
   /**
-   * \brief make a pointer that will be deleted
+   * \brief make a pointer that will be deleted later
    */
   template <typename ...Args>
   static Ptr New(Args... args);
@@ -72,90 +85,13 @@ public:
   /// \brief delete ptr if needed and set all Ptr with the same ptr at nullptr
   static void Delete(Ptr& ptr);
 
-  /// \brief build the pointer through a variable. The Ptr won't be deleted but will be set at null as soon as ~Ptr is called
+  /// \brief build the pointer through a variable. The Ptr won't be deleted but will be set at nullptr as soon as ~Ptr is called
   static Ptr Adr(T& variable);
 
+  /// \brief if the pointer wasn't just created, it will set out the Ptr from the list
   virtual ~Ptr();
 };
 
-template <typename T>
-Ptr<T>::Ptr(const Ptr& cpy) {
-  m_ptr = cpy.m_ptr;
-  toDelete = cpy.toDelete;
-  m_id = list[m_ptr].size();
-  list[m_ptr].push_back(this);
-}
 
-template <typename T>
-Ptr<T>::Ptr(const Ptr&& mv) {
-  m_ptr = mv.m_ptr;
-  toDelete = mv.toDelete;
-  m_id = list[m_ptr].size();
-  list[m_ptr].push_back(this);
-}
-
-template <typename T>
-Ptr<T>::~Ptr() {
-  if (newest) {
-    newest = false;
-  } else {
-    pre_dtor();
-  }
-}
-
-template <typename T>
-Ptr<T>& Ptr<T>::operator=(const Ptr &cpy) {
-  pre_dtor();
-  m_ptr = cpy.m_ptr;
-  m_id = list[m_ptr].size();
-  toDelete = cpy.toDelete;
-  list[m_ptr].push_back(this);
-  return *this;
-}
-
-template <typename T>
-Ptr<T>& Ptr<T>::operator=(const Ptr<T> &&mv) {
-  pre_dtor();
-  m_ptr = mv.m_ptr;
-  m_id = list[m_ptr].size();
-  toDelete = mv.toDelete;
-  list[m_ptr].push_back(this);
-  return *this;
-}
-
-template <typename T>
-Ptr<T>& Ptr<T>::operator=(std::nullptr_t null) {
-  if (null != nullptr) throw "TODO: throw bad adresse exception";
-  pre_dtor();
-  m_id = -1;
-  m_ptr = nullptr;
-  toDelete = false;
-  return *this;
-}
-
-template <typename T>
-template<typename ...Args>
-Ptr<T> Ptr<T>::New(Args... args) {
-  auto ptr = new T(args...);
-  return Ptr(ptr, true);
-}
-
-template <typename T>
-Ptr<T> Ptr<T>::Adr(T& variable) {
-  return Ptr(&variable, false);
-}
-
-template <typename T>
-void Ptr<T>::Delete(Ptr& ptr) {
-  if (ptr.toDelete) {
-    delete ptr.m_ptr;
-  }
-  for (auto* p : list[ptr.m_ptr]) {
-    p->m_ptr = nullptr;
-    p->m_id = -1;
-    p->toDelete = false;
-  }
-
-}
 
 #include "Ptr.hxx"
--- SafePtr/Ptr.hxx
+++ SafePtr/Ptr.hxx
@@ -1,1 +1,143 @@
 
+
+template <typename T>
+Ptr<T>::Ptr(const Ptr& cpy) {
+  m_ptr = cpy.m_ptr;
+  toDelete = cpy.toDelete;
+  m_id = list[m_ptr].size();
+  list[m_ptr].push_back(this);
+}
+
+template <typename T>
+Ptr<T>::Ptr(const Ptr&& mv) {
+  m_ptr = mv.m_ptr;
+  toDelete = mv.toDelete;
+  m_id = list[m_ptr].size();
+  list[m_ptr].push_back(this);
+}
+
+template <typename T>
+Ptr<T>::~Ptr() {
+  if (newest) {
+    newest = false;
+  } else {
+    pre_dtor();
+  }
+}
+
+template <typename T>
+Ptr<T>& Ptr<T>::operator=(const Ptr &cpy) {
+  pre_dtor();
+  m_ptr = cpy.m_ptr;
+  m_id = list[m_ptr].size();
+  toDelete = cpy.toDelete;
+  list[m_ptr].push_back(this);
+  return *this;
+}
+
+template <typename T>
+Ptr<T>& Ptr<T>::operator=(const Ptr<T> &&mv) {
+  pre_dtor();
+  m_ptr = mv.m_ptr;
+  m_id = list[m_ptr].size();
+  toDelete = mv.toDelete;
+  list[m_ptr].push_back(this);
+  return *this;
+}
+
+template <typename T>
+Ptr<T>& Ptr<T>::operator=(std::nullptr_t null) {
+  if (null != nullptr) throw bad_access_ptr();
+  pre_dtor();
+  m_id = -1;
+  m_ptr = nullptr;
+  toDelete = false;
+  return *this;
+}
+
+
+
+template <typename T>
+template<typename ...Args>
+Ptr<T> Ptr<T>::New(Args... args) {
+  auto ptr = new T(args...);
+  return Ptr(ptr, true);
+}
+
+template <typename T>
+Ptr<T> Ptr<T>::Adr(T& variable) {
+  return Ptr(&variable, false);
+}
+
+template <typename T>
+void Ptr<T>::Delete(Ptr& ptr) {
+  if (ptr.toDelete) {
+    delete ptr.m_ptr;
+  }
+  for (auto* p : list[ptr.m_ptr]) {
+    p->m_ptr = nullptr;
+    p->m_id = -1;
+    p->toDelete = false;
+  }
+
+}
+
+
+template <typename T>
+bool Ptr<T>::operator==(const Ptr& ptr) const {
+  return m_ptr == ptr.m_ptr;
+}
+
+template <typename T>
+bool Ptr<T>::operator==(const Ptr&& ptr) const {
+  return  m_ptr == ptr.m_ptr;
+}
+
+template <typename T>
+bool Ptr<T>::operator==(const T* ptr) const {
+  return m_ptr == ptr;
+}
+
+
+template <typename T>
+Ptr<T> Ptr<T>::operator+(const int i) const {
+  return Ptr(m_ptr + i, false);
+}
+
+template <typename T>
+Ptr<T> Ptr<T>::operator-(const int i) const {
+  return Ptr(m_ptr - i, false);
+}
+
+template <typename T>
+Ptr<T>& Ptr<T>::operator++() {
+  return *this;
+}
+
+template <typename T>
+Ptr<T>& Ptr<T>::operator--() {
+  return *this;
+}
+
+
+template <typename T>
+Ptr<T>::operator bool() const {
+  return m_ptr == nullptr;
+}
+
+template <typename T>
+const T* Ptr<T>::operator->() const {
+  if (!m_ptr) throw bad_access_ptr();
+    return m_ptr;
+}
+
+template <typename T>
+T* Ptr<T>::operator->() {
+  return m_ptr;
+}
+
+template <typename T>
+const T& Ptr<T>::operator*() const {
+  if (!m_ptr) throw bad_access_ptr();
+  return *m_ptr;
+}
